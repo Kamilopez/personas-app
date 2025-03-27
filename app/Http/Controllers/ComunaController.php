66451535fbdd33ed7d36c98ad0bd58527b60fae4
Kamilopez <kamilopezk123@gmail.com>
2025-03-27 00:15:12 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -67,10 +67,17 @@ class ComunaController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
      */
     public function edit(string $id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+            return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
     }
 
     /**
@@ -78,7 +85,18 @@ class ComunaController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
