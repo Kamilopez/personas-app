783ff7de2868dc1f139aa8736df5605bc062636c
Kamilopez <kamilopezk123@gmail.com>
2025-03-27 19:52:57 -0500

    DepartamentoController Actualizado

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -9,6 +9,6 @@ class Departamento extends Model
 {
     use HasFactory;
     protected $table = 'tb_departamento';
-    protected $primaryKey = 'pais_codi';
+    protected $primaryKey = 'depa_codi';
     public $timestamps = false;
 }
