b1b55f525802e64e4ec1d72f920563b6427749ed
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2025-05-20 18:01:42 -0500

    correccion router api

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\api\MunicipioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
